eef69c257a95f7d903f2140bce04b7fd53ad43be
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-02-26 09:24:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.java
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -24,16 +24,15 @@ class GFG {
 
 class Solution {
     static int evenlyDivides(int n) {
-        int count=0;
-        int original=n;
-        while(n>0){
-            int digit=n%10;
-            if(digit!=0&&original%digit==0){
-                count ++;
+        int count =0;
+        int temp=n;
+        while(temp>0){
+            int digit = temp%10;
+            if(digit!=0&&n%digit==0){
+                count++;
             }
-            n=n/10;
+            temp=temp/10;
         }
-        
         return count;
         // code here
     }
