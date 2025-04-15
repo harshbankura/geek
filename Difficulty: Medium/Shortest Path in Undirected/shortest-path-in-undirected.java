fb1dfd6c0fee8feb6667e905734a7a3a2ec2647f
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-04-15 19:08:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Path in Undirected/shortest-path-in-undirected.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                adj.get(u).add(v);
+                adj.get(v).add(u); // Since the graph is undirected
+            }
+
+            int src = sc.nextInt();
+
+            Solution obj = new Solution();
+            int[] res = obj.shortestPath(adj, src);
+
+            for (int i = 0; i < res.length; i++) {
+                System.out.print(res[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to find the shortest path from a source node to all other nodes
+    public int[] shortestPath(ArrayList<ArrayList<Integer>> adj, int src) {
+        int n= adj.size();
+        int[] dist = new int[n];
+        Arrays.fill(dist,-1);
+        dist[src]=0;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+        while(!q.isEmpty()){
+            int curr= q.poll();
+            for(int neigh:adj.get(curr)){
+                if(dist[neigh]==-1){
+                    dist[neigh]=dist[curr]+1;
+                    q.add(neigh);
+                }
+            }
+        }
+        return dist;
+        // code here
+    }
+}
