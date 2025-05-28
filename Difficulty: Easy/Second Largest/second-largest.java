337725b248a34a08f38230067b89edbbdbc91857
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-05-28 17:07:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,44 +1,22 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int getSecondLargest(int[] arr) {
-        int largest=arr[0];
-        int slargest=-1;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>largest){
-                slargest=largest;
-                largest=arr[i];
-            }else if(arr[i]>slargest&&arr[i]<largest){
-                slargest=arr[i];
+        int large=Integer.MIN_VALUE;
+        int slarge=Integer.MIN_VALUE;
+        for(int i:arr){
+            if(i>large){
+                slarge=large;
+                large=i;
+            }else if(i<large&&i>slarge){
+                slarge=i;
             }
+            
         }
-        return slargest;
-        // Code Here
+        if(slarge==Integer.MIN_VALUE){
+            return -1;
+        }else{
+            return slarge;
+        }
+        // code here
+        
     }
 }
