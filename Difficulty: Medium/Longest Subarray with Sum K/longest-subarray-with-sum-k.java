5354a8c11215bc9ccda2b5725eb260f0abc0ce43
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-06-01 10:18:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -1,56 +1,25 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
-
-        while (t-- > 0) {
-            String line = read.readLine().trim(); // Read the array input
-            String[] numsStr = line.split(" ");   // Split the input string by spaces
-            int[] nums =
-                new int[numsStr.length]; // Convert string array to integer array
-            for (int i = 0; i < numsStr.length; i++) {
-                nums[i] = Integer.parseInt(numsStr[i]);
-            }
-
-            int k = Integer.parseInt(read.readLine().trim()); // Read target sum
-
-            Solution ob = new Solution();
-            int ans = ob.longestSubarray(nums, k); // Call the function and store result
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
-
+import java.util.*;
 class Solution {
     public int longestSubarray(int[] arr, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        int maxLen=0;
-        int sum=0;
+        int max=0;
+        int cur=0;
         for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-            if(sum==k){
-                maxLen=Math.max(maxLen,i+1);
+            cur+=arr[i];
+            if(cur==k){
+                max=i+1;
             }
-            if(map.containsKey(sum-k)){
-                maxLen=Math.max(maxLen,i-map.get(sum-k));
+            if(map.containsKey(cur-k)){
+                int index=map.get(cur-k);
+                max=Math.max(max,i-index);
             }
-            if(!map.containsKey(sum)){
-                map.put(sum,i);
+            if(!map.containsKey(cur)){
+                map.put(cur,i);
             }
-            
         }
-        return maxLen;
-        // code here
+        return max;
+        
         
     }
 }
