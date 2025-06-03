b03bc9a2d548d5d33ba9217cfe05e609c031443e
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-06-03 08:03:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
@@ -1,67 +1,20 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.HashMap;
-
-
-// } Driver Code Ends
-
 class Solution {
 
-    static int findFloor(int[] arr, int k) {
-        int low=0;
-        int high=arr.length-1;
+    static int findFloor(int[] arr, int x) {
         int ans=-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]<=k){
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]<=x){
                 ans=mid;
-                low=mid+1;
+                left=mid+1;
             }else{
-                high=mid-1;
+                right=mid-1;
             }
         }
         return ans;
         // write code here
+        
     }
 }
-
-
-
-//{ Driver Code Starts.
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            int k = Integer.parseInt(br.readLine());
-            // Create Solution object and find closest sum
-            Solution ob = new Solution();
-            int ans = ob.findFloor(arr, k);
-
-            System.out.print(ans);
-
-            System.out.println(); // New line after printing the results
-        }
-    }
-}
-
-// } Driver Code Ends
