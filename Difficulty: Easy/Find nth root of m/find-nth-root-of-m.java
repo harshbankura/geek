b42ed4f11fc2370e15a643e8ea61f0fbdb57a8dc
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-06-03 17:03:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find nth root of m/find-nth-root-of-m.java
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.java
@@ -1,53 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            int m = Integer.parseInt(br.readLine().trim());
-            Solution ob = new Solution();
-            int ans = ob.nthRoot(n, m);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
     public int nthRoot(int n, int m) {
-        int low=1;
+        int low=0;
         int high=m;
         while(low<=high){
-            int mid=(low+high)/2;
-            long ans=power(mid,n);
-            if(ans==m) return mid;
-            if(ans<m){
-                low=mid+1;
-            }else{
+            int mid=low+(high-low)/2;
+            long pow=power(mid,n,m);
+            if(pow==m)return mid;
+            else if(pow>m){
                 high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return -1;
+        
         // code here
+        
     }
-    private static long power(int mid,int n){
+    private long power(int num,int n,int m){
         long ans=1;
-        for(int i=0;i<n;i++){
-            ans=ans*mid;
-            
+        for(int i=1;i<=n;i++){
+            ans*=num;
+            if(ans>m)return ans;
         }
         return ans;
     }
