c9fe5453c4e6b9031f61e00479ee89722699fccf
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-07-02 16:36:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -1,59 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution obj = new Solution();
-
-            // calling maxSubarraySum() function
-            System.out.println(obj.maxSubarraySum(arr));
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-
-    // arr: input array
-    // Function to find the sum of contiguous subarray with maximum sum.
     int maxSubarraySum(int[] arr) {
-         int maxCurrent = arr[0];
-        int maxGlobal = arr[0];
-
-        // Loop through the array
-        for (int i = 1; i < arr.length; i++) {
-            maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
-            maxGlobal = Math.max(maxGlobal, maxCurrent);
+        int curSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            
+            curSum+=arr[i];
+            maxSum=Math.max(maxSum,curSum);
+            if(curSum<0){
+                curSum=0;
+            }
         }
-
-        return maxGlobal;
-    
-
+        return maxSum;
         // Your code here
+        
     }
 }
