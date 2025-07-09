8166d27655878b4d77d461feb7c4f59562a7b78a
harshbankura <144467437+harshbankura@users.noreply.github.com>
2025-07-09 10:28:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Number of Nodes in a Binary Tree/count-number-of-nodes-in-a-binary-tree.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+class Solution {
+
+    public static int countNodes(Node root) {
+        if(root==null){
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left+right+1;
+        // Code here
+        
+    }
+}
